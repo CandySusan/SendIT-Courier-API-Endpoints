ca5f924e7ea6c325c983adb24f383a79a95d627b
Candy Susan <candysusan55@gmail.com>
2018-11-15 02:11:03 +0300

    Fixes Feadback from LFA

--- tests/test_views.py
+++ tests/test_views.py
@@ -61,7 +61,7 @@ class TestApi(unittest.TestCase):
     def test_get_specific_parcel_by_parcelId(self):
         result = self.client.get(self.hostname + 'parcels'+str(self.parcels['parcelId']))
         self.assertEqual(result.status_code,200)
-        # self.assertTrue(order.add_parcel_delivery_order(parcelId),True)
+     
     def test_to_cancel_specific_parcel_delivery_order(self):
         response = self.client.put(self.hostname + 'parcels/1')
         self.assertEqual(response.status_code,405)
@@ -73,33 +73,19 @@ class TestApi(unittest.TestCase):
        
         self.assertEqual(response.status_code, 200)
 
-    # def test_parcelId_mising(self,parcelId): 
-    #     order = Order(PickUp_Location='PickUp_Location', Destination='Destination',
-    #     Price='Price',Status='Status',PaymentMode='PaymentMode',
-    #     No_Of_Deliveries='No_Of_Deliveries',Date='Date', parcelId=1,)
-    #     for parcel in Order.get_parcel_inventory():
-    #         if parcel(parcelId) == 0:
-    #             return parcelId
-    #     self.assertEqual(Order.add_parcel_delivery_order(parcelId),False)
-    
-    # def test_parceId_is_int(self,parcelId):
-    #     order = Order(PickUp_Location='PickUp_Location', Destination='Destination',
-    #     Price='Price',Status='Status',PaymentMode='PaymentMode', 
-    #     No_Of_Deliveries='No_Of_Deliveries',Date='Date')
-    #     for parcel in Order.get_parcel_inventory():
-    #         if type(parcelId) == int:
-    #             return parcelId
-        
-    #     self.assertFalse(Order.add_parcel_delivery_order(parcelId),False)
-    
-    # def test_parcelId_exists(self,parcelId):
-    #     parcelId = 1
-    #     self.assertEqual(add_parcel_delivery_order(parcelId),True)
+class TestValidation(unittest.TestCase):
 
-    # def test_input_is_integer(self,parcelId):
-    #     if type(parcelId) == int:
-    #         return parcelId
-    #     self.assertTrue(add_parcel_delivery_order(parcelId),True)    
+    def test_userId_mising(self):
+        for user in user_list.get("user"):
+            if user.get("userId")== 0:
+                return user
+                
+        self.assertEqual(get_user_with_specific_userId(userId),False)
+    
+    def test_userId_is_int(self):
+        userId = "str"
+        self.assertFalse(get_user_with_specific_userId(userId),False)
+   
 
 class TestUsers(unittest.TestCase):
 
@@ -107,29 +93,17 @@ class TestUsers(unittest.TestCase):
         self.client = app.test_client()
         self.hostname = "http://localhost:5000/api/v1/"
 
-    # def test_user_login(self):
-    #     users = {
-    #         'username': 'candy',
-    #         'email': 'candysusan55@gmail.com',
-    #         'password': 'golda@2020'
-    #     }
-    #     response = self.client.post(self.hostname +'users',
-    #         data=json.dumps(users)
-    #     )
+    def test_user_login(self):
+        users = {
+            'username': 'candy',
+            'email': 'candysusan55@gmail.com',
+            'password': 'golda@2020'
+        }
+        response = self.client.post(self.hostname +'users',
+            data=json.dumps(users)
+        )
         
        
-    #     self.assertEqual(response.status_code, 201)
-
-    # def test_userId_mising(self):
-    #     userId == 0
-    
-    #     self.assertEqual(get_user_with_specific_userId(userId),False)
-    
-    # def test_userId_is_int(self):
-    #     userId = "str"
-    #     self.assertFalse(get_user_with_specific_userId(userId),False)
-
-
-    
+        self.assertEqual(response.status_code, 201)
 
    
